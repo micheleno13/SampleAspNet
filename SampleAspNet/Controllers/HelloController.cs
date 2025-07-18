15ec8eccb33a6737ed135a053d3497f3efea7ca3
micheleno13 <micheleno13@gmail.com>
2025-07-19 06:15:10 +0900

    ビューの基本

--- SampleAspNet/Controllers/HelloController.cs
+++ SampleAspNet/Controllers/HelloController.cs
@@ -3,11 +3,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SampleAspNet.Controllers
 {
-   public class HelloController : Controller
+    public class HelloController : Controller
     {
         public IActionResult Index()
         {
             return Content("Hello, World!");
         }
+
+        public IActionResult Show()
+        {
+            ViewData["Message"] = "Welcome to the Hello Controller!";
+            return View();
+        }        
     }
 }
